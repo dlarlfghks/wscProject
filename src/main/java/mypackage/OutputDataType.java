144e907d6a48bd733bf496174c8296d9fb87f1e9
BoyoungLee <efdf7744@koreatech.ac.kr>
2015-11-25 12:53:39 +0900

    weatherTest

---
+++ src/main/java/mypackage/OutputDataType.java
@@ -0,0 +1,67 @@
+
+package mypackage;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for outputDataType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="outputDataType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="items" type="{}itemsType" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "outputDataType", propOrder = {
+    "items"
+})
+public class OutputDataType {
+
+    protected List<ItemsType> items;
+
+    /**
+     * Gets the value of the items property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the items property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getItems().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link ItemsType }
+     * 
+     * 
+     */
+    public List<ItemsType> getItems() {
+        if (items == null) {
+            items = new ArrayList<ItemsType>();
+        }
+        return this.items;
+    }
+
+}
--- src/test/java/weatherTest.java
+++ src/test/java/weatherTest.java
@@ -1,5 +1,90 @@
-/**
- * Created by user on 2015-11-25.
- */
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import koreatech.cse.service.UserService;
+//import mypackage.MntInfoType;
+//import mypackage.ResultType;
+import org.apache.ibatis.annotations.ResultType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(
+        {"file:src/main/resources/common/services.xml",
+                "file:src/main/resources/common/security.xml",
+                "file:src/main/resources/common/mybatis.xml"}
+)
 public class weatherTest {
+    @Autowired
+    private WebApplicationContext wac;
+    @Value("${env.text}")
+    private String envText;
+
+    @Before
+    public void setup() {
+        System.out.println("wac: " + wac);
+        System.out.println("this: " + this);
+    }
+
+    @Test
+    public void weatherTest() {
+        String sanUrl = "http://know.nifos.go.kr/openapi/mtweather/mountListSearch.do";
+        String sanKey = "4337298518263458129990035415471445925404";
+        int pageUnit = 10;
+        int pageIndex = 1;
+
+        // POJO 클래스의 List 멤버 위에 붙여야 할 어노테이션
+        // @JacksonXmlElementWrapper(useWrapping=false)
+        // protected List<MntInfoType> mntInfo;
+
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(sanUrl)
+                    .queryParam("keyValue", sanKey)
+                    .queryParam("pageUnit", pageUnit)
+                    .queryParam("pageIndex", 1);
+
+            System.out.println("san : " + builder.build().encode().toUri());
+
+            ResultType resultType = restTemplate.getForObject(builder.build().encode().toUri(),
+                    ResultType.class);
+
+            //String totalCnt = resultType.getTotalCnt();
+            //System.out.println(totalCnt);
+
+           //System.out.println(resultType.getMntInfo().get(0).getMntiName());
+
+        }catch (HttpClientErrorException e) {
+            System.out.println("Exception : " + e.getStatusCode() + ": " + e.getStatusText());
+        }
+    }
+
+
+    @After
+    public void wrapup() {
+        System.out.println("wrapup");
+        System.out.println();
+    }
 }
